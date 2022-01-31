f5beb6d83805cdbb740a0ebea27ab13725c6c1e6
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-02-01 00:11:58 +0100

    6 style

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -10,8 +10,10 @@ export default function RecipeCard({ recipe }) {
         {/* image - thumb */}
         <Image
           src={`https:${thumbnail.fields.file.url}`}
-          width={thumbnail.fields.file.details.image.width}
-          height={thumbnail.fields.file.details.image.height}
+          width={300}
+          //   width={thumbnail.fields.file.details.image.width}
+          height={300}
+          //   height={thumbnail.fields.file.details.image.height}
         />
       </div>
       <div className="content">
@@ -25,6 +27,42 @@ export default function RecipeCard({ recipe }) {
           </Link>
         </div>
       </div>
+
+      <style jsx>{`
+        .card {
+          transform: rotateZ(-1deg);
+        }
+        .content {
+          background: #fff;
+          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+          margin: 0;
+          position: relative;
+          top: -40px;
+          left: -10px;
+        }
+        .info {
+          padding: 16px;
+        }
+        .info h4 {
+          margin: 4px 0;
+          text-transform: uppercase;
+        }
+        .info p {
+          margin: 0;
+          color: #777;
+        }
+        .actions {
+          margin-top: 20px;
+          display: flex;
+          justify-content: flex-end;
+        }
+        .actions a {
+          color: #fff;
+          background: #f01b29;
+          padding: 16px 24px;
+          text-decoration: none;
+        }
+      `}</style>
     </div>
   );
 }
